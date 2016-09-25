1dd37c6a601f4e44181f6deda88a19487f323db4
Inderjeet Yadav <inderjeet_yadav@outlook.com>
2016-09-25 15:58:20 -0400

    routes pushed

--- Library_Room_Booking_App/config/routes.rb
+++ Library_Room_Booking_App/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :bookings
-  resources :rooms
+  resources :bookings, :only => [:index,	:new, :create, :edit, :update, :destroy]
+  resources :rooms, :only => [:index,	:new, :create, :edit, :update, :destroy]
   #get 'admin/index'
 
   #get 'sessions/new'
@@ -17,8 +17,37 @@ Rails.application.routes.draw do
   #get 'sessions/new'
 
   post 'users/sign_up_member'
+  post 'users/sign_up_admin'
+  #get 'users/show_admins'
+  post 'users/show_admins'
+  post 'users/show_members'
+  post 'users/Destroy'
   post 'admin/sign_out'
   post 'admin/edit_user_details'
+  get 'admin/member_page'
+  get 'admin/admin_page'
+
+  post 'admin/create_room'
+  post 'admin/show_all_rooms'
+  post 'admin/admin_new'
+  post 'admin/show_all_admin'
+  post 'admin/find_rooms'
+  post 'admin/show_my_bookings'
+
+  post 'bookings/search_room'
+  post 'bookings/search_rooms'
+  #post 'bookings/book_room'
+  get  'bookings/book_room'
+  post 'bookings/find_rooms'
+  post 'bookings/my_bookings'
+  get 'bookings/my_bookings'
+  #get 'bookings/rooms_available'
+  post 'bookings/rooms_available'
+  delete 'bookings/destroy' => 'bookings#destroy'
+
+  match 'rooms/room_schedule' => 'rooms#room_schedule', via: [:get, :post]
+  #post 'rooms/room_schedule' => 'rooms#room_schedule'
+  #map.connect 'rooms/room_schedule', :controller => 'rooms', :action => 'room_schedule', :conditions => { :method => :get }
 
   #get 'admin' => 'admin#index'
   get 'admin/index'
